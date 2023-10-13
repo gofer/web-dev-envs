98e3d9b8a67e5500253a072e262b402ecdc0c154
gofer <gofer@ex-studio.info>
2023-10-13 15:49:33 +0900

    リバースプロキシ用に認証局を修正

--- ca/entrypoint.sh
+++ ca/entrypoint.sh
@@ -5,37 +5,43 @@ if [ "$SECRET_FILE" != '' -a -f "$SECRET_FILE" ]; then
   source "$SECRET_FILE"
 fi
 
-sh $SCRIPT_DIR/generate_root
-if [ $? -ne 0 ]; then
-  echo "Can't build root CA." > /dev/stderr
-  exit 1;
+if [ ! -d $ROOT_CA_DIR ]; then
+  sh $SCRIPT_DIR/generate_root
+  if [ $? -ne 0 ]; then
+    echo "Can't build root CA." > /dev/stderr
+    exit 1
+  fi
 fi
 
-sh $SCRIPT_DIR/generate_intermediate
-if [ $? -ne 0 ]; then
-  echo "Can't build intermediate CA." > /dev/stderr
-  exit 1;
+if [ ! -d $INTER_CA_DIR ]; then
+  sh $SCRIPT_DIR/generate_intermediate
+  if [ $? -ne 0 ]; then
+    echo "Can't build intermediate CA." > /dev/stderr
+    exit 1
+  fi
 fi
 
 sh $SCRIPT_DIR/generate_crl
 if [ $? -ne 0 ]; then
   echo "Can't generate CRL files" > /dev/stderr
-  exit 1;
+  exit 1
 fi
 
-nginx -c /etc/nginx/nginx.conf
+if [ $(pgrep nginx | wc -l) -eq 0 ]; then
+  nginx -c /etc/nginx/nginx.conf
+fi
 
 sh $SCRIPT_DIR/ocsp_root &
 if [ $? -ne 0 ]; then
   echo "Can't start root CA OCSP responder." > /dev/stderr
-  exit 1;
+  exit 1
 fi
 echo $! > /run/ocsp_root.pid
 
 sh $SCRIPT_DIR/ocsp_inter &
 if [ $? -ne 0 ]; then
   echo "Can't start intermediate CA OCSP responder." > /dev/stderr
-  exit 1;
+  exit 1
 fi
 echo $! > /run/ocsp_inter.pid
 
