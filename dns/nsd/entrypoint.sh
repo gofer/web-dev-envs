0906350a20511fcbdeeef1bb931f12f9a547483b
Gofer <gofer@users.noreply.github.com>
2023-10-20 09:36:33 +0900

    いくつかの修正 (#11)
    - `docker-compose.yml`に`deploy`設定を追加
    - nsdの`entrypoint.sh`を修正して、`/tmp/zones`が既に削除されているときの対応を実施した。

--- dns/nsd/entrypoint.sh
+++ dns/nsd/entrypoint.sh
@@ -3,12 +3,14 @@
 TEMP_DIR='/tmp/zones'
 ZONE_DIR='/etc/nsd/zones'
 
-for file in $(ls $TEMP_DIR | grep .zone$); do
-  if [ ! -f "$ZONE_DIR/$file" ]; then
-    cp "$TEMP_DIR/$file" "$ZONE_DIR/$file"
-  fi
-done
+if [ -d "$TEMP_DIR" ]; then
+  for file in $(ls "$TEMP_DIR" | grep .zone$); do
+    if [ ! -f "$ZONE_DIR/$file" ]; then
+      cp "$TEMP_DIR/$file" "$ZONE_DIR/$file"
+    fi
+  done
 
-rm -rf $TEMP_DIR
+  rm -rf "$TEMP_DIR"
+fi
 
 exec $@
